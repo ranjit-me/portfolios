b99342ea2ea0fcc58b69cbfa7f4fb283900cb8c0
Ranjit <ranjitbichukale11@gmail.com>
2026-02-01 08:05:14 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Ranjit Bichukale</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} RB</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
